d3a2a18d7179190a4a324b8ae01f6193f3c2d348
unknown <alireza.shahabi@snapp.cab>
2020-11-15 14:45:44 +0330

    ADD some include and data features

--- src/useJs.js
+++ src/useJs.js
@@ -1,17 +1,18 @@
 (function(window, document){
 
+    const includeRegex = /\$include\('(.*?)'\)/g;
+
     const USE = function() {
         this._routeMap = {};
     }
 
-    const routeObject = function(controller, route, path) {
-        this.controller = controller;
+    const routeObject = function(route, path) {
         this.route = route;
         this.path = path;
     }
 
-    USE.prototype.addRoute = function(controller, route, path) {
-        this._routeMap[route] = new routeObject(controller, route, path);
+    USE.prototype.addRoute = function(route, path) {
+        this._routeMap[route] = new routeObject(route, path);
     }
 
     USE.prototype.mount = function(elementId = 'app') {
@@ -27,19 +28,41 @@
 
     const updateView = function(elementId = 'app') {
         const windowHash = window.location.hash.replace('#', '');
-        const {path, controller} = this._routeMap[windowHash] || {};
+        const {path} = this._routeMap[windowHash] || {};
         fetchFile(path)
         .then(result => {
-            document.getElementById(elementId).innerHTML = result ? injectDataToView(result.toString(), controller()) : '<h1>404 Not found</h1>';
+            const data = compileFile(result);
+            document.getElementById(elementId).innerHTML = data;
+            [...document.getElementById(elementId).querySelectorAll('include')].forEach(item => {
+                const path = item.getAttribute('src');
+                fetchFile(path)
+                .then(res => {
+                    item.insertAdjacentHTML('afterend', compileFile(res));
+                    item.remove();
+                })
+            })
+        })
+        .catch(() => {
+            document.getElementById(elementId).innerHTML =  '<h1>404 Not found</h1>';
         });
     }
 
     const fetchFile = function(path){
         return fetch(path)
                 .then(res => res.text())
+                .then(res => res);
     }
 
-    const injectDataToView = function(string, data) {
+    const compileFile = function(result){
+            const html = document.createElement('body');
+            html.innerHTML = result;
+            const content = html.querySelector('template') || '';
+            const script = html.querySelector('script') || ''
+            const controller = eval(script ? script.innerHTML : '() => {}');
+            return injectDataToView(content ? content.innerHTML.toString() : '', controller());
+    }
+
+    const injectDataToView = function(string = "", data = {}) {
         let stringCopy = string;
         Object.keys(data).forEach(key => {
            stringCopy = stringCopy.replace(`{{${key}}}`, data[key]);
